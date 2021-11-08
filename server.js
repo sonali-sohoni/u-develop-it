92b19110d759a3d2225eca811b7c4e36c0948ffb
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-07 23:29:47 -0500

    CRUD routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 //connect to mysql
 const mysql = require("mysql2");
+const inputCheck = require("./utils/inputCheck");
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
@@ -22,25 +23,98 @@ const db = mysql.createConnection(
 	console.log("database connected successfully")
 );
 
-db.query("select * from candidates where id =1", (err, rows) => {
-	console.log(rows);
-});
-
-// db.query(`delete from candidates where id = ?`, 1, (err, result) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log(result);
-// 	}
+// db.query("select * from candidates where id =1", (err, rows) => {
+// 	console.log(rows);
 // });
 
-const insertsql = `insert into candidates (id,first_name,last_name,industry_connected ) values (?,?,?,?)`;
-const params = [1, "Ronald", "Firbank", 1];
-db.query(insertsql, params, (err, result) => {
-	if (err) console.log(err);
-	else console.log(result);
+// const insertsql = `insert into candidates (id,first_name,last_name,industry_connected ) values (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// db.query(insertsql, params, (err, result) => {
+// 	if (err) console.log(err);
+// 	else console.log(result);
+// });
+
+//api endpoint to get all candidates
+app.get("/api/candidates", (req, res) => {
+	db.query("select * from candidates", (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			console.log(err);
+			return;
+		}
+
+		res.json({
+			message: "success",
+			data: rows,
+		});
+	});
 });
 
+//api endpoint to retrieve information about single candidate with id
+app.get("/api/candidate/:id", (req, res) => {
+	const sql = `select * from candidates where id =?`;
+	const params = [req.params.id];
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.send({
+			message: "success",
+			data: result,
+		});
+	});
+});
+
+app.post("/api/candidate", ({ body }, res) => {
+	const errors = inputCheck(
+		body,
+		"first_name",
+		"last_name",
+		"industry_connected"
+	);
+	if (errors) {
+		res.status(400).json({ error: errors });
+		return;
+	}
+	const sql = `insert into candidates (first_name,last_name,industry_connected) values (?,?,?)`;
+	const params = [body.first_name, body.last_name, body.industry_connected];
+	db.query(sql, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+		res.send({
+			message: "success",
+			data: body,
+		});
+	});
+});
+
+app.delete("/api/candidate/:id", (req, res) => {
+	console.log("You are in delete route");
+	const params = [req.params.id];
+	db.query(`delete from candidates where id = ?`, params, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		} else if (!result.affectedRows) {
+			res.json({
+				message: "candidate not found",
+			});
+		} else {
+			res.send({
+				message: "deleted",
+				changes: result.affectedRows,
+				outcome: result,
+				id: req.params.id,
+			});
+		}
+		console.log(result);
+	});
+});
+
+//middleware to report 404 -page not found for the unhandled requests
 app.use((req, res) => {
 	res.status(404).end();
 });
