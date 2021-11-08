fbb78dfebe96de5fd2a0e8ca7bd3fef543398658
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-08 15:26:17 -0500

    created parties table and inserted data

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
+use election;
+
 create table candidates (
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
+);
+
+
+create table parties(
+id integer auto_increment primary key,
+name varchar(50) not null,
+description text
 );
