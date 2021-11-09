0291444f9baf5386c473eff1497f3a588c5b6202
sonali_sohoni <ssohoni@camsbycbs.com>
2021-11-08 23:26:32 -0500

    code modularization

--- server.js
+++ server.js
@@ -7,60 +7,15 @@ const app = express();
 const mysql = require("mysql2");
 const inputCheck = require("./utils/inputCheck");
 const db = require("./db/connection");
-const apiroutes = require("./routes/apiroutes")
+const apiroutes = require("./routes/apiroutes");
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api", apiroutes);
 
-
-
 //api endpoint to get all candidates
 
 //api for parties
-
-app.get("/api/parties", (req, res) => {
-	const sql = `select * from parties`;
-	db.query(sql, (err, rows) => {
-		if (err) {
-			res.status(500).json({ error: err.message });
-			return;
-		}
-		res.json({ message: "success", data: rows });
-	});
-});
-
-app.get("/api/party/:id", (req, res) => {
-	const sql = `select * from parties where id = ?`;
-	const params = [req.params.id];
-	db.query(sql, params, (err, result) => {
-		if (err) {
-			res.status(400).json({ error: err.message });
-			return;
-		}
-		res.json({ message: "success", data: result });
-	});
-});
-
-app.delete("/api/party/:id", (req, res) => {
-	const sql = `delete from parties where id = ?`;
-	const params = [req.params.id];
-	db.query(sql, params, (err, result) => {
-		if (err) {
-			res.status(400).json({ error: err.message });
-		} else if (!result.affectedRows) {
-			res.json({
-				message: "party not found",
-			});
-		} else {
-			res.json({
-				message: "deleted",
-				changes: result.affectedRows,
-				id: req.params.id,
-			});
-		}
-	});
-});
 
 //
 
@@ -69,6 +24,14 @@ app.use((req, res) => {
 	res.status(404).end();
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is listening to port ${PORT}`);
+db.connect((err) => {
+	if (err) throw err;
+	console.log("Database connected.");
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
 });
+
+// app.listen(PORT, () => {
+// 	console.log(`Server is listening to port ${PORT}`);
+// });
